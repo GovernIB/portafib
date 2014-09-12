2937ddef814692305a85bd1b252e2bd86e0924b8
anadal-ibit <anadal-ibit@1a83b7bb-df65-4973-8540-7514423c1e25>
2014-09-12 11:28:25 +0000

    Primer Commit
    git-svn-id: https://svn.code.sf.net/p/portafib/code-0/portafib-core/branches/portafib-1.0@21 1a83b7bb-df65-4973-8540-7514423c1e25

--- dozip.bat
+++ dozip.bat
@@ -3,4 +3,4 @@
 for /f "tokens=* delims=" %%x in (versio.txt) do set PORTAFIB_VERSIO=%%x
 
 
-zip -r portafib_%PORTAFIB_VERSIO%%1.zip scripts doc/pdf lib versio.txt portafib.genapp ear/target/portafib.ear plugins/plugins-documentcustody/filesystem/index.jsp ws/portafib_api/target/portafib-ws-api-1.0.0.jar ws\indra_api\target\portafib-ws-indra-api-1.0.0.jar ws/indra_callback_api/MCGDWS.wsdl ws/portafib_callback_api/PortaFIBCallBack_v1.wsdl -x "**/.svn**"  -x "scripts/sqlgenenerator/**" -x "scripts/pom.xml" 
+zip -r portafib%1-%PORTAFIB_VERSIO%.zip scripts doc/pdf lib versio.txt portafib.genapp ear/target/portafib.ear plugins/plugins-documentcustody/filesystem/index.jsp ws/portafib_api/target/portafib-ws-api-1.0.0.jar ws/indra_api/target/portafib-ws-indra-api-1.0.0.jar ws/indra_callback_api/src/main/resources/wsdl/PortafirmasCallBack.wsdl ws/portafib_callback_api/src/main/resources/wsdl/PortaFIBCallBack_v1.wsdl -x "**/.svn**"  -x "scripts/sqlgenenerator/**" -x "scripts/pom.xml" 
