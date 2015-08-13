36e93caa68bc3141b138b2b321021479cbfeeccc
anadal-ibit <anadal-ibit@1a83b7bb-df65-4973-8540-7514423c1e25>
2015-08-13 12:23:09 +0000

    Seleccio dinàmica d'usuaris en Petició de flux de firmes (solicitant i admin. d'entitat). Actualitzar documentació. Eliminar opció "Fluxos orfes". Errors varis de PortaFIB.
    git-svn-id: https://svn.code.sf.net/p/portafib/code-0/portafib-core/branches/portafib-1.0@134 1a83b7bb-df65-4973-8540-7514423c1e25

---
+++ back/src/main/java/es/caib/portafib/back/reflist/UsuariEntitatJSONRefList.java
@@ -0,0 +1,64 @@
+package es.caib.portafib.back.reflist;
+
+
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import org.fundaciobit.genapp.common.StringKeyValue;
+import org.fundaciobit.genapp.common.i18n.I18NException;
+import org.fundaciobit.genapp.common.query.Field;
+import org.fundaciobit.genapp.common.query.OrderBy;
+import org.fundaciobit.genapp.common.query.Select;
+import org.fundaciobit.genapp.common.query.SelectConstant;
+import org.fundaciobit.genapp.common.query.Where;
+import org.fundaciobit.genapp.common.web.controller.RefListBase;
+import org.springframework.stereotype.Component;
+
+import es.caib.portafib.ejb.UsuariEntitatLocal;
+import es.caib.portafib.model.fields.UsuariEntitatQueryPath;
+import es.caib.portafib.model.fields.UsuariPersonaQueryPath;
+
+/**
+ * 
+ * @author anadal
+ *
+ */
+@Component
+public class UsuariEntitatJSONRefList extends RefListBase {
+  
+  @EJB(mappedName = UsuariEntitatLocal.JNDI_NAME)
+  private UsuariEntitatLocal usuariEntitatEjb;
+
+
+  /**
+   * 
+   */
+  public UsuariEntitatJSONRefList() {
+    super();
+    init();
+  }
+
+  
+  protected void init() {
+    
+
+    final UsuariPersonaQueryPath personaQueryPath = new UsuariEntitatQueryPath().USUARIPERSONA();
+    this.setSelects(new Select[] { 
+        personaQueryPath.LLINATGES().select , new SelectConstant(", "), 
+        personaQueryPath.NOM().select, new SelectConstant(" ("), 
+        personaQueryPath.NIF().select, new SelectConstant(" - "),
+        personaQueryPath.USUARIPERSONAID().select,new SelectConstant(")") });
+    
+    this.setSeparator("");
+  }
+  
+  
+  public List<StringKeyValue> getReferenceList(Field<?> keyField, Where where, OrderBy ... orderBy) throws I18NException {
+    Select<StringKeyValue> select =  new org.fundaciobit.genapp.common.query.SelectMultipleStringKeyValue(keyField.select, getSeparator(), getSelects());
+    List<StringKeyValue> list = usuariEntitatEjb.executeQuery(select, where, (orderBy==null || orderBy.length == 0) ? getOrderBy() : orderBy);
+    return list;
+  }
+  
+  
+}
