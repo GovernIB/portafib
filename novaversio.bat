4dd32e763a3dd90ebc6937e1314121436beda4b5
anadal-fundaciobit <anadal@fundaciobit.org>
2016-09-29 09:26:18 +0200

    Canviar versió a 1.1.2

--- novaversio.bat
+++ novaversio.bat
@@ -6,15 +6,33 @@ IF "%~1" == "" GOTO SENSEPARAMS
 @echo on
 cmd /C mvn -Pws-portafib -Pws-portafib-callback-server -Pws-portafirmas -Pws-portafirmas-callback-server -Psqlgen -Pclientcert  -Pdesenvolupament -Pproduccio -DgroupId=es.caib.portafib -DartifactId=* versions:set -DnewVersion=%*
 
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
 @echo. 
 @echo. 
-@echo --------------------------- IMPORTANT ---------------------------------
-@echo ^|  Has d'actualitzar la propietat portafib.version del pom.xml arrel  ^|
-@echo -----------------------------------------------------------------------
+call :ColorText 4F "--------------------------- IMPORTANT ---------------------------------"
+@echo. 
+call :ColorText 4F "[  Has d'actualitzar la propietat portafib.version del pom.xml arrel  ]"
+@echo. 
+call :ColorText 4F "-----------------------------------------------------------------------"
+
 @echo.
 @echo.
 @echo off
+
 GOTO EXIT
+
+:ColorText
+@echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
+
 
 :SENSEPARAMS
 
