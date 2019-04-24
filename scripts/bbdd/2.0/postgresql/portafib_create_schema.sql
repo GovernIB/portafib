a4b862c140ea81824b7a51ad048f13ba372b5ab3
areus-fundaciobit <areus@fundaciobit.org>
2019-04-24 13:39:21 +0200

    afegir creació schema i configuració variables postgresql #253

--- scripts/bbdd/2.0/postgresql/portafib_create_schema.sql
+++ scripts/bbdd/2.0/postgresql/portafib_create_schema.sql
@@ -1,4 +1,18 @@
 
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE SCHEMA portafib;
+ALTER SCHEMA portafib OWNER TO portafib;
+
+SET search_path = portafib, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
     create table pfi_annex (
         annexid int8 not null,
         adjuntar bool not null,
