205ad525ba78ce067696fbdb7d6c3a4e4c0b74f4
anadal-fundaciobit <anadal@fundaciobit.org>
2019-05-15 10:49:15 +0200

    Solució per HTTPS amb ClientCert #259

--- logic/src/main/java/es/caib/portafib/logic/PluginValidacioFirmesLogicaLocal.java
+++ logic/src/main/java/es/caib/portafib/logic/PluginValidacioFirmesLogicaLocal.java
@@ -1,8 +1,5 @@
 package es.caib.portafib.logic;
 
-import java.io.File;
-import java.io.InputStream;
-
 import javax.ejb.Local;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
@@ -10,7 +7,6 @@ import org.fundaciobit.plugins.validatesignature.api.IValidateSignaturePlugin;
 import org.fundaciobit.plugins.validatesignature.api.ValidateSignatureResponse;
 
 import es.caib.portafib.logic.utils.datasource.IPortaFIBDataSource;
-import es.caib.portafib.model.entity.UsuariAplicacioConfiguracio;
 
 /**
  * 
@@ -23,25 +19,8 @@ public interface PluginValidacioFirmesLogicaLocal extends
 
   public static final String JNDI_NAME = "portafib/PluginValidacioFirmesLogicaEJB/local";
 
-  public ValidateSignatureResponse validateSignatureWeb(final String entitatID,
-      String signType, File signatureFile, InputStream documentDetachedIS, 
-      String languageUI) throws I18NException;
-
-  public ValidateSignatureResponse validateSignatureInServer(final String entitatID, 
-      final UsuariAplicacioConfiguracio usuariAplicacioConfig,
-      final String signatureType, final byte[] signedFile, byte[] documentDetachedFile,
-      final String languageUI) throws I18NException;
-
-  
-  public ValidateSignatureResponse validateSignaturePassarela(final String entitatID,
-      final UsuariAplicacioConfiguracio usuariAplicacioConfig,
-      String signType, File signatureFile, InputStream documentDetachedIS, 
-      String languageUI) throws I18NException;
-  
-
-  public ValidateSignatureResponse validateSignature(final String entitatID,
-      String signType, IPortaFIBDataSource signature, 
-      IPortaFIBDataSource documentDetached, String languageUI)
+  public ValidateSignatureResponse validateSignature(final String entitatID, String signType,
+      IPortaFIBDataSource signature, IPortaFIBDataSource documentDetached, String languageUI)
       throws I18NException;
-  
+
 }
