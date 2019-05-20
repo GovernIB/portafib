51a20a964d78b0b9ed2fba1abe672471c8f31d41
anadal-fundaciobit <anadal@fundaciobit.org>
2019-05-20 12:16:56 +0200

    Eliminar deploydev i deploypro de PortaFIB i GenApp #264

--- cleanAll.sh
+++ cleanAll.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env mvn clean -Psqlgen -Pminiappletui -Pclientcert -Pws-portafib-v1 -Pws-portafib-v2 -Pws-portafib-callback-server -Pws-portafirmas  -Pws-portafirmas-callback-server -Pdesenvolupament -Psia -Ppassarelaweb-v1 -Ppassarelaserver-v1 -Ppassarelaweb-v2 -Ppassarelaserver-v2
+env mvn clean -Psqlgen -Pminiappletui -Pws-portafib-v1 -Pws-portafib-v2 -Pws-portafib-callback-server -Pws-portafirmas  -Pws-portafirmas-callback-server -Psia -Ppassarelaweb-v1 -Ppassarelaserver-v1 -Ppassarelaweb-v2 -Ppassarelaserver-v2
