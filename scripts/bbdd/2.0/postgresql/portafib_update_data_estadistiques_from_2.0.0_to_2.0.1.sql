5c6e5d9f69098ec6d28695d20371221d8e127b99
areus-fundaciobit <areus@fundaciobit.org>
2019-08-08 14:39:42 +0200

    scripts d'inicialització de peticions firmades #258

---
+++ scripts/bbdd/2.0/postgresql/portafib_update_data_estadistiques_from_2.0.0_to_2.0.1.sql
@@ -0,0 +1,12 @@
+﻿
+-- Peticions firmades
+INSERT INTO pfi_estadistica(estadisticaid, tipus, data, entitatid, valor, parametres, usuariaplicacioid, usuarientitatid)
+select nextval('pfi_portafib_seq'), 3, p.datafinal, 'fundaciobit', max(f.numfirmadocument),
+ CONCAT('usuariEntitatID=', p.usuarientitatid, chr(10), 'entitatID=', 'fundaciobit', chr(10), 'tipusFirmaID=', p.tipusfirmaid, chr(10), 'peticioDeFirmaID=', p.peticiodefirmaid, chr(10),'tipusDocumentID=', p.tipusdocumentid),
+ p.usuariaplicacioid, p.usuarientitatid
+ from pfi_peticiodefirma p, pfi_blocdefirmes b, pfi_firma f
+ where p.fluxdefirmesid = b.fluxdefirmesid
+   and b.blocdefirmesid = f.blocdefirmaid
+   and p.tipusestatpeticiodefirmaid = 4
+ group by p.peticiodefirmaid;
+ 
