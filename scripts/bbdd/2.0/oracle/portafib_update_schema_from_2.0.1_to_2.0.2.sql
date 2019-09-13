11b36306ed15f303d0265ecc08e7ff194223b61f
areus-fundaciobit <areus@fundaciobit.org>
2019-09-13 10:30:41 +0200

    Afegir UsuariAplicacio a NotificacioWS #332

--- scripts/bbdd/2.0/oracle/portafib_update_schema_from_2.0.1_to_2.0.2.sql
+++ scripts/bbdd/2.0/oracle/portafib_update_schema_from_2.0.1_to_2.0.2.sql
@@ -3,4 +3,15 @@
 
 ALTER TABLE pfi_notificacio
   DROP CONSTRAINT pfi_notifica_petifirma_fk;
-  
+
+-- Afegir la columna amb l'id d'usuari aplicació pel funcionament de les notificacions #332
+
+ALTER TABLE pfi_notificacio
+  ADD usuariaplicacioid varchar2(101 char);
+
+UPDATE pfi_notificacio SET usuariaplicacioid =
+        (SELECT usuariaplicacioid FROM pfi_peticiodefirma
+        WHERE peticiodefirmaid = pfi_notificacio.peticiodefirmaid);
+
+ALTER TABLE pfi_notificacio
+   MODIFY (usuariaplicacioid NOT NULL);
--- scripts/bbdd/2.0/postgresql/portafib_update_schema_from_2.0.1_to_2.0.2.sql
+++ scripts/bbdd/2.0/postgresql/portafib_update_schema_from_2.0.1_to_2.0.2.sql
@@ -4,3 +4,14 @@
 ALTER TABLE pfi_notificacio
   DROP CONSTRAINT pfi_notifica_petifirma_fk;
 
+-- Afegir la columna amb l'id d'usuari aplicació pel funcionament de les notificacions #332
+
+ALTER TABLE pfi_notificacio
+  ADD COLUMN usuariaplicacioid character varying(101);
+
+UPDATE pfi_notificacio SET usuariaplicacioid =
+        (SELECT usuariaplicacioid FROM pfi_peticiodefirma
+        WHERE peticiodefirmaid = pfi_notificacio.peticiodefirmaid);
+
+ALTER TABLE pfi_notificacio
+   ALTER COLUMN usuariaplicacioid SET NOT NULL;
