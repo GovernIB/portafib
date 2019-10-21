3ff455db6751e382ba8178a9a1fbae9dda3e2bd7
anadal-fundaciobit <anadal@fundaciobit.org>
2019-10-21 10:11:02 +0200

    Modificar Flux de Firmes per mostrar usuari extern enlloc d'usuari persona #361

--- back/src/main/java/es/caib/portafib/back/controller/rest/apifirmaasyncsimple/v2/RestApiFirmaAsyncSimpleV2Controller.java
+++ back/src/main/java/es/caib/portafib/back/controller/rest/apifirmaasyncsimple/v2/RestApiFirmaAsyncSimpleV2Controller.java
@@ -1010,17 +1010,28 @@ public class RestApiFirmaAsyncSimpleV2Controller extends
       final String msg = "Els blocs de firmes de la Petició de Firmes val null o està buit";
       throw new I18NException("genapp.comodi", msg);
     } else {
-      jpa.setFluxDeFirmes(toJPA(blocks, entitatJPA.getEntitatID(), fitxersCreats, languageUI));
+      jpa.setFluxDeFirmes(toJPA(blocks, entitatJPA.getEntitatID(), fitxersCreats, 
+          languageUI, signatureRequest.getTitle()));
     }
 
     return jpa;
   }
 
+  /**
+   * 
+   * @param blocks
+   * @param entitatID
+   * @param fitxersCreats
+   * @param languageUI
+   * @param titolPeticio
+   * @return
+   * @throws I18NException
+   */
   protected FluxDeFirmesJPA toJPA(FirmaAsyncSimpleSignatureBlock[] blocks, String entitatID,
-      Set<Long> fitxersCreats, String languageUI) throws I18NException {
+      Set<Long> fitxersCreats, String languageUI, String titolPeticio) throws I18NException {
 
     // Bean
-    FluxDeFirmesJPA jpa = new FluxDeFirmesJPA("Flux per Petició Async " + blocks.toString());
+    FluxDeFirmesJPA jpa = new FluxDeFirmesJPA("Flux per Petició Async " + titolPeticio);
 
     Set<BlocDeFirmesJPA> blocsDeFirmesJPA = new HashSet<BlocDeFirmesJPA>();
     for (int b = 0; b < blocks.length; b++) {
