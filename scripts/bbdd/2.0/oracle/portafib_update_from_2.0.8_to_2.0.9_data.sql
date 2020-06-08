c33080b9f51a7d4d4ac40feb9fcfa7a34197210c
Antoni Reus <areus@fundaciobit.org>
2020-06-08 12:09:35 +0200

    no especificar columnes pot fer fallar insert #147

--- scripts/bbdd/2.0/oracle/portafib_update_from_2.0.8_to_2.0.9_data.sql
+++ scripts/bbdd/2.0/oracle/portafib_update_from_2.0.8_to_2.0.9_data.sql
@@ -8,7 +8,7 @@
 -- Propietats Globals
 -- ========================================
 
-insert into pfi_propietatglobal
+insert into pfi_propietatglobal (propietatglobalid, clau, descripcio, entitatid, valor)
 select pfi_portafib_seq.nextval as propietaglobalid ,'es.caib.portafib.maxpeticiotitlelength' as clau, 
         'Opcional. Valor per defecte 0. Indica la longitud màxima del titol de una peticio de firma si està fixada.' as descripcio,
         entitatid, null as valor
