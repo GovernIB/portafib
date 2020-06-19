b957b64c7810868e44cc8e4f231a31f6b0bed4fb
Antoni Reus <areus@fundaciobit.org>
2020-06-19 09:41:11 +0200

    Modificar gestió de plugins de custòdia d'acord amb #467

--- back/src/main/java/es/caib/portafib/back/controller/admin/PluginDeCustodiaAdminController.java
+++ back/src/main/java/es/caib/portafib/back/controller/admin/PluginDeCustodiaAdminController.java
@@ -15,9 +15,9 @@ import es.caib.portafib.jpa.PluginJPA;
 import es.caib.portafib.utils.ConstantsV2;
 
 /**
- * 
- * @author anadal
  *
+ * @author anadal
+ * @author areus
  */
 @Controller
 @RequestMapping(value = "/admin/plugincustodia")
@@ -43,18 +43,36 @@ public class PluginDeCustodiaAdminController extends AbstractPluginAdminControll
   public String getCodeName() {
     return "plugincustodia";
   }
-  
-  
+
+
   @Override
   public PluginForm getPluginForm(PluginJPA _jpa,
       boolean __isView, HttpServletRequest request, ModelAndView mav) throws I18NException {
      PluginForm pluginForm = super.getPluginForm(_jpa, __isView, request, mav);
+
+     // Els plugins de custòdia no s'asocien amb entitats, per tant ho amagam
+     pluginForm.addHiddenField(ENTITATID);
+     // Atès que no s'asocien a entitats, per tant no hi ha propietats a nivell d'entitat
+     // i no cal mostrar la política de si es poden editar o no des de l'entitat propietats
+     pluginForm.addHiddenField(PROPERTIESENTITAT);
+     pluginForm.addHiddenField(POLITICAMOSTRARPROPIETATS);
+
      if(!pluginForm.isNou()) {
        // TODO XYZ Només fer editable la classe en cas de que no hi hagi peticions
-       // o les peticions no estiguin actives 
+       // o les peticions no estiguin actives
        pluginForm.addReadOnlyField(CLASSE);
      }
 
      return pluginForm;
    }
+
+    @Override
+    public PluginFilterForm getPluginFilterForm(Integer pagina, ModelAndView mav, HttpServletRequest request) throws I18NException {
+        PluginFilterForm pluginFilterForm = super.getPluginFilterForm(pagina, mav, request);
+        if (pluginFilterForm.isNou()) {
+            // Els plugins de custòdia no s'asocien amb entitats, per tant ho amagam
+            pluginFilterForm.addHiddenField(ENTITATID);
+        }
+        return pluginFilterForm;
+    }
 }
