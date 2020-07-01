521dc670e97d9dfeb49346ad2504c7c9b4bbf3b0
Antoni Reus <areus@fundaciobit.org>
2020-07-01 14:30:34 +0200

    Quan la propietat "portafiburlforexternalsignatures" no està definida l'error no es propaga adequadament #473

--- logic/src/main/java/es/caib/portafib/logic/PeticioDeFirmaLogicaEJB.java
+++ logic/src/main/java/es/caib/portafib/logic/PeticioDeFirmaLogicaEJB.java
@@ -1786,7 +1786,7 @@ public class PeticioDeFirmaLogicaEJB extends PeticioDeFirmaEJB
                     msg = "S'ha intentat enviar correu a " + firmaJPA.getUsuariExternEmail()
                         + " però ha fallat: " + msg;
                     log.error(msg, i18ne);
-                    throw new PeticioHaDeSerRebutjadaException(i18ne);
+                    throw new PeticioHaDeSerRebutjadaException(msg);
                   }
 
                 break;
@@ -2631,11 +2631,14 @@ public class PeticioDeFirmaLogicaEJB extends PeticioDeFirmaEJB
       try {
         peticioFinalitzada = startNextSign(peticioDeFirma, flux, estatDeFirma, events);
       } catch (PeticioHaDeSerRebutjadaException e) {
+        // Es correcte deixar rebutjada la firma actual? veure #473
 
-        String motiuDeRebuig = e.getMessage();
+        //String motiuDeRebuig = e.getMessage();
+        //rebutjar(estatDeFirma, firma, peticioDeFirma, motiuDeRebuig, usernameLoguejat);
+        //return;
 
-        rebutjar(estatDeFirma, firma, peticioDeFirma, motiuDeRebuig, usernameLoguejat);
-        return;
+        // de moment optam per rellançar una excepció com si hagués fallat la validació
+        throw new I18NException("genapp.comodi", e.getMessage());
       }
 
       // 7.- Enviar Avisos
--- logic/src/main/java/es/caib/portafib/logic/utils/PeticioHaDeSerRebutjadaException.java
+++ logic/src/main/java/es/caib/portafib/logic/utils/PeticioHaDeSerRebutjadaException.java
@@ -3,17 +3,13 @@ package es.caib.portafib.logic.utils;
 import org.fundaciobit.genapp.common.i18n.I18NException;
 
 /**
- * 
+ * Indica que una petició ha de ser rebutjada. El message de l'excepció és el missage de rebuig.
  * @author anadal(u80067)
- *
+ * @author areus
  */
 public class PeticioHaDeSerRebutjadaException extends Exception {
 
-  protected final I18NException i18nException;
-
-  public PeticioHaDeSerRebutjadaException(I18NException i18nException) {
-    super();
-    this.i18nException = i18nException;
+  public PeticioHaDeSerRebutjadaException(String message) {
+    super(message);
   }
-
 }
