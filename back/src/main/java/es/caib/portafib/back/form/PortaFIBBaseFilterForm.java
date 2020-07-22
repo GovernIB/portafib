b5aad8b6befad10ac60a203e964abffb4de3cd43
Antoni Reus <areus@fundaciobit.org>
2020-07-22 11:35:08 +0200

    L'opció TOTS dels llistats s'hauria d'inhabilitar/canviar #446

--- back/src/main/java/es/caib/portafib/back/form/PortaFIBBaseFilterForm.java
+++ back/src/main/java/es/caib/portafib/back/form/PortaFIBBaseFilterForm.java
@@ -5,21 +5,17 @@ import org.fundaciobit.genapp.common.web.form.BaseFilterForm;
 /**
  * 
  * @author anadal
- *
+ * @author areus
  */
 public abstract class PortaFIBBaseFilterForm extends BaseFilterForm {
-  
-  
-  
+
   public PortaFIBBaseFilterForm() {
     setVisibleExportList(false);
+    setAllItemsPerPage(new int[] {10, 20, 50, 100});
   }
-  
-  
-  
+
   public PortaFIBBaseFilterForm(PortaFIBBaseFilterForm __toClone) {
     super(__toClone);
   }
-  
 
 }
