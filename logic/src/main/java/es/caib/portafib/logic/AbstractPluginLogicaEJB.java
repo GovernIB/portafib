17483bc770421fb2de7a00afb0f9e649a49dbf6c
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2020-10-05 12:10:00 +0200

    Substitucio de contrassenyes de plugins per propietats properties-service.xml #328

--- logic/src/main/java/es/caib/portafib/logic/AbstractPluginLogicaEJB.java
+++ logic/src/main/java/es/caib/portafib/logic/AbstractPluginLogicaEJB.java
@@ -8,11 +8,19 @@ import java.util.Properties;
 import es.caib.portafib.jpa.PluginJPA;
 import es.caib.portafib.model.entity.Plugin;
 import es.caib.portafib.utils.ConstantsV2;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.fundaciobit.genapp.common.query.Where;
 import org.fundaciobit.pluginsib.core.IPlugin;
 import org.fundaciobit.pluginsib.core.utils.PluginsManager;
+import org.fundaciobit.pluginsib.utils.templateengine.TemplateEngine;
 
 /**
  *
@@ -20,136 +28,157 @@ import org.fundaciobit.pluginsib.core.utils.PluginsManager;
  *
  */
 public abstract class AbstractPluginLogicaEJB<I extends IPlugin> extends PluginLogicaEJB
-   implements AbstractPluginLogicaLocal<I> {
+        implements AbstractPluginLogicaLocal<I> {
 
-  protected abstract int getTipusDePlugin();
-  
-  protected abstract String getName();
-  
-  
-  @Override
-  public List<Plugin> getAllPlugins(String entitatID) throws I18NException {
-    Where where = getWhere(entitatID);
-    return select(where);
-  }
+    protected abstract int getTipusDePlugin();
 
-  @Override
-  public List<Plugin> getAllPluginsSenseEntitat() throws I18NException {
-    Where where = Where.AND(
-            TIPUS.equal(getTipusDePlugin()),
-            ACTIU.equal(true),
-            ENTITATID.isNull());
-    return select(where);
-  }
+    protected abstract String getName();
 
-  public Where getWhere(String entitatID) {
-    return Where.AND(
-        TIPUS.equal(getTipusDePlugin()),
-        ACTIU.equal(true),
-        ENTITATID.equal(entitatID)
+    @Override
+    public List<Plugin> getAllPlugins(String entitatID) throws I18NException {
+        Where where = getWhere(entitatID);
+        return select(where);
+    }
+
+    @Override
+    public List<Plugin> getAllPluginsSenseEntitat() throws I18NException {
+        Where where = Where.AND(
+                TIPUS.equal(getTipusDePlugin()),
+                ACTIU.equal(true),
+                ENTITATID.isNull());
+        return select(where);
+    }
+
+    public Where getWhere(String entitatID) {
+        return Where.AND(
+                TIPUS.equal(getTipusDePlugin()),
+                ACTIU.equal(true),
+                ENTITATID.equal(entitatID)
         // TODO Elegim plugin entre les genèriques o entre els específics per l'entitat
         // Where.OR(ENTITATID.isNull(), ENTITATID.equal(entitatID))
-    );
-  }
-  
-  @Override
-  public I getInstanceByPluginID(long pluginID) throws I18NException {
+        );
+    }
 
-    IPlugin pluginInstance = getPluginFromCache(pluginID);
+    @Override
+    public I getInstanceByPluginID(long pluginID) throws I18NException {
 
-    if (pluginInstance == null) {
+        IPlugin pluginInstance = getPluginFromCache(pluginID);
 
-      PluginJPA plugin = (PluginJPA) findByPrimaryKey(pluginID);
+        if (pluginInstance == null) {
 
-      if (plugin == null) {
-        return null;
-      }
+            PluginJPA plugin = (PluginJPA) findByPrimaryKey(pluginID);
 
-      Properties prop = new Properties();
+            if (plugin == null) {
+                return null;
+            }
 
-      if (plugin.getPropertiesAdmin() != null
-          && plugin.getPropertiesAdmin().trim().length() != 0) {
-        try {
+            Properties prop = new Properties();
 
-          prop.load(new StringReader(plugin.getPropertiesAdmin()));
+            if (plugin.getPropertiesAdmin() != null
+                    && plugin.getPropertiesAdmin().trim().length() != 0) {
+                try {
 
-        } catch (Exception e) {
-          // TODO Crec que no es cridarà mai
+                    prop.load(new StringReader(plugin.getPropertiesAdmin()));
+
+                } catch (Exception e) {
+                    // TODO Crec que no es cridarà mai
+                }
+            }
+
+            if (plugin.getPropertiesEntitat() != null
+                    && plugin.getPropertiesEntitat().trim().length() != 0) {
+                try {
+
+                    prop.load(new StringReader(plugin.getPropertiesEntitat()));
+
+                } catch (Exception e) {
+                    // TODO Crec que no es cridarà mai
+                }
+            }
+
+            try {
+
+                //log.info("PROP");
+                StringWriter writer = new StringWriter();
+                prop.store(writer, "");
+                String propietats = writer.getBuffer().toString().replace("[\\=", "[=");
+                //log.info(propietats);
+
+                Map<String, Object> parameters = new HashMap<String, Object>();
+                parameters.put("SP", System.getProperties());
+
+                //log.info("CONTINGUT SP");
+                //log.info(parameters.get("SP"));
+                String plantilla = propietats;
+
+                //log.info("Previo");
+                //log.info(plantilla);
+                //log.info("");
+                String t = TemplateEngine.processExpressionLanguageSquareBrackets(plantilla, parameters);
+
+                //log.info("Tranformada");
+                //log.info(t);
+                prop.load(new ByteArrayInputStream(t.getBytes()));
+
+                //log.info("PROP 2");
+                //writer = new StringWriter();
+                //prop.store(writer, "");
+                //propietats = writer.getBuffer().toString();
+                //log.info(propietats);
+            } catch (IOException ex) {
+                Logger.getLogger(AbstractPluginLogicaEJB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            pluginInstance = (IPlugin) PluginsManager.instancePluginByClassName(plugin.getClasse(),
+                    ConstantsV2.PORTAFIB_PROPERTY_BASE, prop);
+
+            if (pluginInstance == null) {
+                throw new I18NException("plugin.donotinstantiate", getName() + " ("
+                        + plugin.getClasse() + ")");
+            }
+
+            addPluginToCache(pluginID, pluginInstance);
+
         }
-      }
-
-      if (plugin.getPropertiesEntitat() != null
-          && plugin.getPropertiesEntitat().trim().length() != 0) {
-        try {
-
-          prop.load(new StringReader(plugin.getPropertiesEntitat()));
-
-        } catch (Exception e) {
-          // TODO Crec que no es cridarà mai
-        }
-      }
-      pluginInstance = (IPlugin) PluginsManager.instancePluginByClassName(plugin.getClasse(),
-          ConstantsV2.PORTAFIB_PROPERTY_BASE, prop);
-
-      if (pluginInstance == null) {
-        throw new I18NException("plugin.donotinstantiate", getName() + " ("
-            + plugin.getClasse() + ")");
-      }
-
-      addPluginToCache(pluginID, pluginInstance);
+        return (I) pluginInstance;
 
     }
-    return (I) pluginInstance;
 
-  }
-  
-  @Override
-  public List<I> getPluginInstancesByEntitatID(String entitatID) throws I18NException {
-    
-     return getPluginInstancesBy(entitatID, null, null);
-    
-  }
-  
-  
-  
-  @Override
-  public List<I> getPluginInstancesBy(String entitatID, List<Long> filterByPluginID,
-      List<String> filterByPluginCode) throws I18NException {
-   
-    
-    
-    List<I> plugins = new ArrayList<I>();
-    
-    Where where = Where.AND(
-        TIPUS.equal(getTipusDePlugin()),
-        ACTIU.equal(true),
-        ENTITATID.equal(entitatID)
-    );
-    
-    
-    if (filterByPluginID != null && filterByPluginID.size() != 0) {
-      where = Where.AND(where, PLUGINID.in(filterByPluginID));
+    @Override
+    public List<I> getPluginInstancesByEntitatID(String entitatID) throws I18NException {
+
+        return getPluginInstancesBy(entitatID, null, null);
+
     }
-    
-    // TODO XYZ pendent afegir camp codi dins plugin
-    //    if (filterByPluginCode != null && filterByPluginCode.size() != 0) {
-    //      where = Where.AND(where, CODI.in(filterByPluginID));
-    //    }
-    
-    
-    List<Plugin> modulsdefirma = select(where);
-    
-    
-    for (Plugin mf : modulsdefirma) {
-      plugins.add(getInstanceByPluginID(mf.getPluginID()));
-    } 
-    
-    return plugins;
 
-  }
+    @Override
+    public List<I> getPluginInstancesBy(String entitatID, List<Long> filterByPluginID,
+            List<String> filterByPluginCode) throws I18NException {
 
-  
+        List<I> plugins = new ArrayList<I>();
 
-  
-  
+        Where where = Where.AND(
+                TIPUS.equal(getTipusDePlugin()),
+                ACTIU.equal(true),
+                ENTITATID.equal(entitatID)
+        );
+
+        if (filterByPluginID != null && filterByPluginID.size() != 0) {
+            where = Where.AND(where, PLUGINID.in(filterByPluginID));
+        }
+
+        // TODO XYZ pendent afegir camp codi dins plugin
+        //    if (filterByPluginCode != null && filterByPluginCode.size() != 0) {
+        //      where = Where.AND(where, CODI.in(filterByPluginID));
+        //    }
+        List<Plugin> modulsdefirma = select(where);
+
+        for (Plugin mf : modulsdefirma) {
+            plugins.add(getInstanceByPluginID(mf.getPluginID()));
+        }
+
+        return plugins;
+
+    }
+
 }
