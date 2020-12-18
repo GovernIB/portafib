06a0737d73d104207122e4609b21d7764d324a63
Antoni Reus <areus@fundaciobit.org>
2020-12-18 11:23:20 +0100

    Obrir Versió 2.0.15 #432

--- ws/portafib_api/src/test/java/es/caib/portafib/ws/v1/test/PortaFIBUsuariAplicacioTest.java
+++ ws/portafib_api/src/test/java/es/caib/portafib/ws/v1/test/PortaFIBUsuariAplicacioTest.java
@@ -73,9 +73,9 @@ public final class PortaFIBUsuariAplicacioTest extends PortaFIBTestUtils {
   @Test
   public void testVersio() throws Exception {
     if (isCAIB()) {
-      Assert.assertEquals("2.0.14-caib", usuariAplicacioAPI.getVersion());
+      Assert.assertEquals("2.0.15-caib", usuariAplicacioAPI.getVersion());
     } else {
-      Assert.assertEquals("2.0.14", usuariAplicacioAPI.getVersion());
+      Assert.assertEquals("2.0.15", usuariAplicacioAPI.getVersion());
     }
   }
 
