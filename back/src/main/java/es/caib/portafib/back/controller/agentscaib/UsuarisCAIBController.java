971ee2cc83f7e023a419d37bbe2aaae076e30032
Antoni Nadal <anadal@fundaciobit.org>
2024-10-07 12:31:07 +0200

    Tornar a activar els serveis web per donar d'alta i baixa des d'Agents Seycon #896

--- back/src/main/java/es/caib/portafib/back/controller/agentscaib/UsuarisCAIBController.java
+++ back/src/main/java/es/caib/portafib/back/controller/agentscaib/UsuarisCAIBController.java
@@ -10,11 +10,13 @@ import es.caib.portafib.logic.AgentsCAIBLocal;
 import es.caib.portafib.logic.utils.PropietatGlobalUtil;
 import es.caib.portafib.model.entity.UsuariEntitat;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+
 import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.util.*;
 
 /**
  *
@@ -23,69 +25,65 @@ import java.util.*;
 @Controller
 public class UsuarisCAIBController {
 
-  protected final Logger log = Logger.getLogger(getClass());
+    protected final Logger log = Logger.getLogger(getClass());
 
-  @EJB(mappedName = AgentsCAIBLocal.JNDI_NAME)
-  protected AgentsCAIBLocal carrecsCaibEjb;
+    @EJB(mappedName = AgentsCAIBLocal.JNDI_NAME)
+    protected AgentsCAIBLocal carrecsCaibEjb;
 
-  protected static final Locale locale = new Locale("ca");
+    protected static final Locale locale = new Locale("ca");
 
-  @RequestMapping(value = "/usuariscaib", method = RequestMethod.GET)
-  @ResponseBody
-  public void index(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "/usuariscaib", method = RequestMethod.GET)
+    @ResponseBody
+    public void index(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-    log.info(" ==================== Entra dins INDEX USUARIS ========================");
+        log.info(" ==================== Entra dins INDEX USUARIS ========================");
 
-    if (log.isDebugEnabled()) {
-      Map<String, String[]> params = request.getParameterMap();
-      for (String key : params.keySet()) {
-        log.debug("Param[" + key + "]: " + Arrays.toString(params.get(key)));
-      }
+        if (log.isDebugEnabled()) {
+            Map<String, String[]> params = request.getParameterMap();
+            for (String key : params.keySet()) {
+                log.debug("Param[" + key + "]: " + Arrays.toString(params.get(key)));
+            }
+        }
+
+        String tipus = request.getParameter("tipus");
+
+        String codusu = request.getParameter("codusu");
+        //String nomrol = request.getParameter("nomrol");
+        //String valordomini = request.getParameter("valordomini");
+        String agentsql = request.getParameter("agentsql");
+        //String nom = request.getParameter("nom");
+        String password = request.getParameter("password");
+
+        // Check password
+        String passwordOK = PropietatGlobalUtil.getPasswordForAgentsSQL();
+
+        if (passwordOK == null) {
+            log.error("S'ha de definir la propietat es.caib.portafib.passwordforagentssql !!!!!");
+            response.getOutputStream().println("ERROR");
+            return;
+        }
+
+        if (!passwordOK.trim().equals(password)) {
+            log.error("Algu està intentant accedir a la Gestió d'Usuaris via AgentSeycon però"
+                    + " la contrasenya passada no s'ajusta al valor definit dins la propietat"
+                    + " es.caib.portafib.passwordforagentssql:\n" + "  + getRemoteAddr: " + request.getRemoteAddr()
+                    + "\n" + "  + RemoteHost: " + request.getRemoteHost() + "\n" + "  + RemotePort: "
+                    + request.getRemotePort() + "\n");
+            response.getOutputStream().println("ERROR");
+            return;
+        }
+
+        UsuariEntitat ue;
+        ue = carrecsCaibEjb.processarUsuariCAIB(tipus, codusu, agentsql, password);
+
+        if (ue == null) {
+            response.getOutputStream().println("ERROR");
+        } else {
+            response.getOutputStream().println("OK: " + ue.getUsuariEntitatID());
+        }
+
+        log.info(" ------------------ Final INDEX USUARIS ------------------");
+
     }
-
-    String tipus = request.getParameter("tipus");
-
-    String codusu = request.getParameter("codusu");
-    //String nomrol = request.getParameter("nomrol");
-    //String valordomini = request.getParameter("valordomini");
-    String agentsql = request.getParameter("agentsql");
-    //String nom = request.getParameter("nom");
-    String password = request.getParameter("password");
-    
-    // Check password
-    
-    String passwordOK = PropietatGlobalUtil.getPasswordForAgentsSQL();
-    
-    if (passwordOK == null) {
-      log.error("S'ha de definir la propietat es.caib.portafib.passwordforagentssql !!!!!");
-      response.getOutputStream().println("ERROR");
-      return;
-    }
-    
-    if (!passwordOK.trim().equals(password)) {
-      log.error("Algu està intentant accedir a la Gestió d'Usuaris via AgentSeycon però"
-          + " la contrasenya passada no s'ajusta al valor definit dins la propietat"
-          + " es.caib.portafib.passwordforagentssql:\n"
-          + "  + getRemoteAddr: " + request.getRemoteAddr() + "\n"
-          + "  + RemoteHost: " + request.getRemoteHost() + "\n"
-          + "  + RemotePort: " + request.getRemotePort() + "\n");
-      response.getOutputStream().println("ERROR");
-      return;
-    } 
-    
-
-    UsuariEntitat ue;
-    ue = carrecsCaibEjb
-        .processarUsuariCAIB(tipus, codusu, agentsql, password);
-
-    if (ue == null) {
-      response.getOutputStream().println("ERROR");
-    } else {
-      response.getOutputStream().println("OK: " + ue.getUsuariEntitatID());
-    }
-
-    log.info(" ------------------ Final INDEX USUARIS ------------------");
-
-  }
 
 }
