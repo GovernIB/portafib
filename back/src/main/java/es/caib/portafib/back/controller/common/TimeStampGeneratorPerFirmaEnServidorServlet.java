546d35df6be7911bc91f099453136257bca902ae
Antoni Nadal <anadal@fundaciobit.org>
2025-01-28 14:14:28 +0100

    Plugin de Firma En Servidor utilitzant llibreries Integr@ llança error al activar Segellat de Temps. #950

--- back/src/main/java/es/caib/portafib/back/controller/common/TimeStampGeneratorPerFirmaEnServidorServlet.java
+++ back/src/main/java/es/caib/portafib/back/controller/common/TimeStampGeneratorPerFirmaEnServidorServlet.java
@@ -2,6 +2,8 @@ package es.caib.portafib.back.controller.common;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.Calendar;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -10,10 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.bouncycastle.tsp.TimeStampRequest;
+import org.bouncycastle.tsp.TimeStampToken;
 import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.fundaciobit.genapp.common.web.i18n.I18NUtils;
 import org.fundaciobit.pluginsib.signatureserver.api.ISignatureServerPlugin;
+import org.fundaciobit.pluginsib.timestamp.api.ITimeStampPlugin;
 import org.fundaciobit.pluginsib.core.v3.utils.FileUtils;
+import org.fundaciobit.pluginsib.signature.api.FileInfoSignature;
+import org.fundaciobit.pluginsib.signature.api.ITimeStampGenerator;
 import org.fundaciobit.pluginsib.utils.webutils.AbstractWebPlugin;
 import org.springframework.stereotype.Controller;
 
@@ -33,8 +40,11 @@ public class TimeStampGeneratorPerFirmaEnServidorServlet extends HttpServlet {
 
     protected Logger log = Logger.getLogger(getClass());
 
-    @EJB(mappedName = ModulDeFirmaServidorLogicaLocal.JNDI_NAME)
-    protected ModulDeFirmaServidorLogicaLocal modulDeFirmaEnServidorEjb;
+    //@EJB(mappedName = ModulDeFirmaServidorLogicaLocal.JNDI_NAME)
+    //protected ModulDeFirmaServidorLogicaLocal modulDeFirmaEnServidorEjb;
+
+    @EJB(mappedName = SegellDeTempsLogicaLocal.JNDI_NAME)
+    protected SegellDeTempsLogicaLocal modulDeSegellDeTempsEjb;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -55,8 +65,8 @@ public class TimeStampGeneratorPerFirmaEnServidorServlet extends HttpServlet {
             log.debug(AbstractWebPlugin.servletRequestInfoToStr(request));
         }
 
-        //                                {plugin}/signaturesSetID/signIndex
-        // uri = /common/timestampgenerator/666/1466408733012148444/0
+        //                              {plugin}/signaturesSetID/signIndex
+        // uri = /common/timestampgenerator/666/1466408733012144/0
         String uri = request.getRequestURI();
 
         if (debug) {
@@ -85,10 +95,124 @@ public class TimeStampGeneratorPerFirmaEnServidorServlet extends HttpServlet {
             }
         }
 
-        long pluginID = Long.parseLong(split[0]);
-        String signaturesSetID = split[1];
-        int signatureIndex = Integer.parseInt(split[2]);
+        final long pluginID = Long.parseLong(split[0]);
+        final String signaturesSetID = split[1];
+        final int signatureIndex = Integer.parseInt(split[2]);
 
+        ITimeStampPlugin segellDeTempsPlugin;
+        try {
+            segellDeTempsPlugin = modulDeSegellDeTempsEjb.getInstanceByPluginID(pluginID);
+        } catch (I18NException e) {
+            String msg = I18NUtils.tradueix("plugin.segelldetemps.noexist", String.valueOf(pluginID));
+            log.error(msg);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, msg);
+            return;
+        }
+        if (segellDeTempsPlugin == null) {
+            String msg = I18NUtils.tradueix("plugin.segelldetemps.noexist", String.valueOf(pluginID));
+            log.error(msg);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, msg);
+            return;
+        }
+
+        InputStream is = request.getInputStream();
+        byte[] inputRequest = FileUtils.toByteArray(is);
+
+        TimeStampRequest tsr = new TimeStampRequest(inputRequest);
+
+        byte[] inputData = tsr.getMessageImprintDigest();
+
+        BigInteger time = tsr.getNonce();
+
+        try {
+            final Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(time.longValue());
+            byte[] returnedData = segellDeTempsPlugin.getTimeStampDirect(inputData, cal);
+
+            if (returnedData != null) {
+                log.info("requestTimeStamp:: returnedData LEN= " + returnedData.length);
+                log.info("requestTimeStamp:: returnedData DATA= " + new String(returnedData));
+                response.getOutputStream().write(returnedData);
+            } else {
+                String msg = "El generador de TimeStamp per la petició " + signaturesSetID + "[" + signatureIndex
+                        + "] ha retornat null";
+                log.error(msg, new Exception());
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+
+        } catch (Exception e) {
+            log.error("Error greu cridant el TimeStampGenerator: " + e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+        /*
+        try {
+            TimeStampRequest tsr = new TimeStampRequest(inputRequest);
+        
+            BigInteger time = tsr.getNonce();
+        
+            final Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(time.longValue());
+            // 0 No ha funcionat
+            // byte[] inputData = tsr.getMessageImprintDigest();
+            // byte[] timeStamp = segellDeTempsPlugin.getTimeStampDirect(inputData, cal);  
+        
+            //  0No ha funcionat
+            // byte[] inputData = tsr.getEncoded();
+            // byte[] timeStamp = segellDeTempsPlugin.getTimeStampDirect(inputData, cal);  
+        
+            
+        
+            // 1 No ha funcionat
+            //byte[] inputData = tsr.getMessageImprintDigest();
+            //TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            //byte[] timeStamp = tst.getEncoded();
+            
+            // 1 No ha funcionat
+            //byte[] inputData = tsr.getEncoded();
+            //TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            //byte[] timeStamp = tst.getEncoded();
+            
+            // 2 No ha funcionat
+            //byte[] inputData = tsr.getMessageImprintDigest();
+            //TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            //byte[] timeStamp = tst.getTimeStampInfo().getMessageImprintDigest();
+            
+            
+            // 2 No ha funcionat
+            //byte[] inputData = tsr.getEncoded();
+            //TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            //byte[] timeStamp = tst.getTimeStampInfo().getMessageImprintDigest();
+            
+            // 3 No ha funcionat
+            //byte[] inputData = tsr.getMessageImprintDigest();
+            //TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            //byte[] timeStamp = tst.getTimeStampInfo().getEncoded();
+            
+            // 3 No ha funcionat
+            byte[] inputData = tsr.getEncoded();
+            TimeStampToken tst = segellDeTempsPlugin.getTimeStamp(inputData, cal);
+            byte[] timeStamp = tst.getTimeStampInfo().getEncoded();
+        
+            if (timeStamp == null) {
+                String msg = "El generador de TimeStamp per la petició " + signaturesSetID + "[" + signatureIndex
+                        + "] ha retornat null";
+                log.error(msg);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                response.getOutputStream().write(timeStamp);
+            }
+        
+        } catch (Exception e) {
+            String msg = "Error greu cridant el TimeStampGenerator per la petició " + signaturesSetID + "["
+                    + signatureIndex + "]: " + e.getMessage();
+            log.error(msg, e);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        
+        }
+        */
+
+        /*
         ISignatureServerPlugin signaturePlugin;
         try {
             signaturePlugin = modulDeFirmaEnServidorEjb.getInstanceByPluginID(pluginID);
@@ -104,14 +228,14 @@ public class TimeStampGeneratorPerFirmaEnServidorServlet extends HttpServlet {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, msg);
             return;
         }
-
+        
         InputStream is = request.getInputStream();
         byte[] inputRequest = FileUtils.toByteArray(is);
-
+        
         byte[] timeStamp;
         try {
             timeStamp = signaturePlugin.generateTimeStamp(signaturesSetID, signatureIndex, inputRequest);
-
+        
             if (timeStamp == null) {
                 String msg = "El generador de TimeStamp per la petició " + signaturesSetID + "[" + signatureIndex
                         + "] ha retornat null";
@@ -120,14 +244,15 @@ public class TimeStampGeneratorPerFirmaEnServidorServlet extends HttpServlet {
             } else {
                 response.getOutputStream().write(timeStamp);
             }
-
+        
         } catch (Exception e) {
             String msg = "Error greu cridant el TimeStampGenerator per la petició " + signaturesSetID + "["
                     + signatureIndex + "]: " + e.getMessage();
             log.error(msg, e);
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
-
+        
         }
+        */
     }
 
 }
