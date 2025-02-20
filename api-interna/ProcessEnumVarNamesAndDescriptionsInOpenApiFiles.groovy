7de3f132addca33ba8d8b3fc69470ed098790f76
Antoni Nadal <anadal@fundaciobit.org>
2025-02-20 08:15:09 +0100

    Canviar tipus de dades de "SignTypes" de String a Enumerats a la API de firma #967

---
+++ api-interna/ProcessEnumVarNamesAndDescriptionsInOpenApiFiles.groovy
@@ -0,0 +1,59 @@
+/**
+ *  @author anadal
+ *  @date 20/02/2025
+ */
+
+import java.nio.file.*
+import groovy.json.*
+
+def processExampleLine(pattern, line) {
+    def matcher = line =~ pattern
+    if (matcher.find()) {
+        log.info(" =======  GROUP[1] => " + matcher.group(1))
+        log.info(" =======  SPLIT => " + matcher.group(1).split('\\|'))
+        def values = matcher.group(1).split('\\|').collect { "\"$it\"" }.join(', ')
+        log.info(" =======  VALUES => " + values)
+        return "\"x-enum-varnames\" : [ " + values + " ],"
+    }
+    return null
+}
+
+log.info('\n\n   -------------- PROCESSANT ENUMs EN OPENAPI FILES  -----------------:\n\n')
+
+def dirPath = properties['openapifolder']
+log.info(" Open Api Folder: " + dirPath)
+
+def dir = new File(dirPath)
+
+if (dir.exists() && dir.isDirectory()) {
+    dir.eachFileMatch(~/.*\.json/) { file ->
+        if (file.name != "swaggerui_urls.json") {
+
+            def inputFile = file  // Nombre del archivo de entrada
+            def outputFile = file //'output.txt' // Nombre del archivo de salida
+            
+            def lines = Files.readAllLines(Paths.get(file.absolutePath))
+            def updatedLines = []
+            
+            def pattern = /"example" *: *"([^"]+)"/  // Patrón para capturar el valor de "example"
+
+            lines.eachWithIndex { line, index ->
+                //log.info(line); 
+                if (line.trim().startsWith('"example"') && index + 1 < lines.size() && lines[index + 1].trim().startsWith('"enum"')) {
+                    def newLine = processExampleLine(pattern, line)
+                    if (newLine) {
+                        updatedLines << newLine
+                    }
+                } else {
+                    updatedLines << line
+                }
+            }
+            
+            Files.write(Paths.get(file.absolutePath), updatedLines)
+            log.info("Arxiu " + file.name + " processat correctament.\n")
+
+        }
+    }
+} else {
+    log.error("El directorio " + dirPath + "no existe o no es válido.")
+}
