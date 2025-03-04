6b45796f380367eb633d8fcd7cdbef1184cab76b
fbosch-fundaciobit <fbosch@fundaciobit.org>
2025-03-04 08:57:18 +0100

    Simplificar APIResponses en els Services de PortaFIB #970(Revisio)

---
+++ api-interna/src/main/java/es/caib/portafib/api/interna/secure/firma/v1/commons/SignAlgorithmConstants.java
@@ -0,0 +1,28 @@
+package es.caib.portafib.api.interna.secure.firma.v1.commons;
+
+import org.springframework.context.annotation.Description;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Description("Hola")
+@Schema(
+        name = "SignAlgorithmConstants",
+        description = "Algorisme de Firma. Valors:\r\n" + "    • \"SHA-1\"\r\n" + "    • \"SHA-256\"\r\n"
+                + "    • \"SHA-384\"\r\n" + "    • \"SHA-512\"",
+        enumAsRef = true,
+        examples = "SHA-1|SHA-256|SHA-384|SHA-512")
+public enum SignAlgorithmConstants {
+    SIGN_ALGORITHM_CONSTANTS_SHA1("SHA-1"), SIGN_ALGORITHM_CONSTANTS_SHA256("SHA-256"),
+    SIGN_ALGORITHM_CONSTANTS_SHA384("SHA-384"), SIGN_ALGORITHM_CONSTANTS_SHA512("SHA-512");
+
+    public final String value;
+
+    SignAlgorithmConstants(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
