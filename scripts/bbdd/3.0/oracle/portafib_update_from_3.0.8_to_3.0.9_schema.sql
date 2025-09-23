da396b75e9a61a739e07abd88365a23e12c97dbd
Antoni Nadal <anadal@fundaciobit.org>
2025-09-23 15:19:20 +0200

    Tancar Versió 3.0.x #823

---
+++ scripts/bbdd/3.0/oracle/portafib_update_from_3.0.8_to_3.0.9_schema.sql
@@ -0,0 +1,49 @@
+
+
+
+--######################################################################
+--##### 28/08/2025 Gestió via BBDD de Correus Agrupats #1063 
+--######################################################################
+
+
+CREATE SEQUENCE pfi_correuagrupat_seq  START WITH 1000 increment by 1;
+
+CREATE TABLE pfi_correuagrupat
+(
+   correuagrupatid number(19,0) DEFAULT pfi_correuagrupat_seq.nextval, 
+   email varchar2(255 char) NOT NULL, 
+   subject varchar2(255 char) NOT NULL, 
+   message clob NOT NULL, 
+   html number(1,0) NOT NULL, 
+   usuarientitatid varchar2(101 char) NOT NULL, 
+   datacreacio timestamp NOT NULL,
+   error clob
+);
+
+alter table pfi_correuagrupat add CONSTRAINT pfi_correuagrupat_pk PRIMARY KEY (correuagrupatid);
+
+alter table pfi_correuagrupat add CONSTRAINT pfi_correagrup_usrentitat_fk FOREIGN KEY (usuarientitatid) REFERENCES pfi_usuarientitat (usuarientitatid);
+
+create index pfi_correagrup_usrentitat_fk_i on pfi_correuagrupat (usuarientitatid);
+
+grant select,insert,delete,update on pfi_correuagrupat to www_portafib;
+
+
+
+
+--######################################################################
+--##### 17/09/2025 Cercar solució al problema d'ignorar validació de NIf en Certificats de Pseudònim #1035
+--######################################################################
+
+CREATE SEQUENCE pfi_pseudonim_seq START WITH 1000 increment by 1;
+
+CREATE TABLE pfi_pseudonim
+(
+   pseudonimid number(19,0) DEFAULT pfi_pseudonim_seq.nextval, 
+   pseudonim varchar2(255 char) NOT NULL, 
+   nif varchar2(255 char) NOT NULL
+);
+
+alter table pfi_pseudonim add CONSTRAINT pfi_pseudonim_pk PRIMARY KEY (pseudonimid);
+
+grant select,insert,delete,update on pfi_pseudonim to www_portafib;
--- scripts/bbdd/3.0/postgresql/portafib_update_from_3.0.8_to_3.0.9_data.sql
+++ scripts/bbdd/3.0/postgresql/portafib_update_from_3.0.8_to_3.0.9_data.sql
@@ -9,37 +9,6 @@ insert into pfi_propietatglobal (propietatglobalid, clau, descripcio, entitatid,
 
 
 --######################################################################
---##### 28/08/2025 Gestió via BBDD de Correus Agrupats #1063 
---######################################################################
-
-CREATE SEQUENCE pfi_correuagrupat_seq
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1000
-  CACHE 1;
-
-CREATE TABLE pfi_correuagrupat
-(
-   correuagrupatid bigint NOT NULL DEFAULT nextval('pfi_correuagrupat_seq'), 
-   email character varying(255) NOT NULL, 
-   subject character varying(255) NOT NULL, 
-   message text NOT NULL, 
-   html boolean NOT NULL, 
-   usuarientitatid character varying(101) NOT NULL, 
-   datacreacio timestamp without time zone NOT NULL,
-   error text,
-   CONSTRAINT pfi_correuagrupat_pk PRIMARY KEY (correuagrupatid), 
-   CONSTRAINT pfi_correagrup_usrentitat_fk FOREIGN KEY (usuarientitatid) REFERENCES pfi_usuarientitat (usuarientitatid) ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-
-
-create index pfi_correuagrupat_pk_i on pfi_correuagrupat (correuagrupatid);
-create index pfi_correagrup_usrentitat_fk_i on pfi_correuagrupat (usuarientitatid);
-
-
-
---######################################################################
 --##### 11/09/2025 Capçaleres d'entitat durant la signatura WEB #1058
 --######################################################################
 
@@ -70,27 +39,3 @@ SELECT nextval('pfi_propietatglobal_seq') as propietaglobalid ,'es.caib.portafib
         entitatid
         FROM pfi_entitat;
 
-
---######################################################################
---##### 17/09/2025 Cercar solució al problema d'ignorar validació de NIf en Certificats de Pseudònim #1035
---######################################################################
-
-
-CREATE SEQUENCE pfi_pseudonim_seq
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1000
-  CACHE 1;
-  
-  
-CREATE TABLE pfi_pseudonim
-(
-   pseudonimid bigint NOT NULL DEFAULT nextval('pfi_pseudonim_seq'), 
-   pseudonim character varying(255) NOT NULL, 
-   nif character varying(255) NOT NULL, 
-   CONSTRAINT pfi_pseudonim_pk PRIMARY KEY (pseudonimid)
-);
-
-create index pfi_pseudonim_pk_i on pfi_pseudonim (pseudonimid);
-  
